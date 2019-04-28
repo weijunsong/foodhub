384d67c5d516394d5deacb5dca49588be6deaaec
weijunsong <50065427+weijunsong@users.noreply.github.com>
2019-04-28 13:08:39 +0800

    Add files via upload

---
+++ FoodDelivery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
